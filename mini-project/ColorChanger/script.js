904b8a69c8c16b5f6686581cbfbbf6a6c4a6a231
moonjonghoo <kafmjh12@gmail.com>
2025-04-13 00:18:04 +0900

    로직개선

--- mini-project/ColorChanger/script.js
+++ mini-project/ColorChanger/script.js
@@ -1,17 +1,20 @@
 const text = document.querySelector(".color-text");
 const background = document.querySelector(".color-changer");
-const changebutton = document.querySelector(".btn-change");
-let color = "";
+const changeButton = document.querySelector(".btn-change");
 
-function rendomColor() {
-  let r = Math.random() * 255;
-  let g = Math.random() * 255;
-  let b = Math.random() * 255;
-  return `rgb(${r},${g},${b})`;
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
-changebutton.addEventListener("click", () => {
-  color = rendomColor();
-  text.innerHTML = color;
-  background.style.background = color;
+function updateBackground(color) {
+  background.style.backgroundColor = color;
+  text.textContent = color;
+}
+
+changeButton.addEventListener("click", () => {
+  const color = randomColor();
+  updateBackground(color);
 });
